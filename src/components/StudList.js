07a6c819a2de91073c0da995acef6a4e9e84b06e
Rahul <rkhurana@csumb.edu>
2022-09-18 16:34:52 -0700

    Adding work for Assignment 3 - created React frontend for Registration Page - added StudList and AddStudent

---
+++ src/components/StudList.js
@@ -0,0 +1,167 @@
+import React, { Component } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Cookies from 'js-cookie';
+import {SERVER_URL} from '../constants.js'
+import Grid from '@mui/material/Grid';
+import {DataGrid} from '@mui/x-data-grid';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import AddStudent from './AddStudent';
+
+// NOTE:  for OAuth security, http request must have
+//   credentials: 'include' 
+//
+
+// properties year, semester required
+//  
+//  NOTE: because StudList is invoked via <Route> in App.js  
+//  props are passed in props.location
+
+class StudList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { students: [] };
+  } 
+  
+  componentDidMount() {
+    this.fetchStudents();
+  }
+  
+  fetchStudents = () => {
+    console.log("StudList.fetchStudents");
+    const token = Cookies.get('XSRF-TOKEN');
+    
+    fetch(`${SERVER_URL}/student`, 
+      {  
+        method: 'GET', 
+        headers: { 'X-XSRF-TOKEN': token }
+      } )
+    .then((response) => {
+      console.log("FETCH RESP:"+response);
+      return response.json();}) 
+    .then((responseData) => { 
+      // do a sanity check on response
+      if (Array.isArray(responseData.courses)) {
+        this.setState({ 
+          courses: responseData.courses,
+        });
+      } else {
+        toast.error("Fetch failed.", {
+          position: toast.POSITION.BOTTOM_LEFT
+        });
+      }        
+    })
+    .catch(err => {
+      toast.error("Fetch failed.", {
+          position: toast.POSITION.BOTTOM_LEFT
+        });
+        console.error(err); 
+    })
+  }
+
+  /*// Drop Course 
+  onDelClick = (id) => {
+    if (window.confirm('Are you sure you want to drop the course?')) {
+      const token = Cookies.get('XSRF-TOKEN');
+      
+      fetch(`${SERVER_URL}/schedule/${id}`,
+        {
+          method: 'DELETE',
+          headers: { 'X-XSRF-TOKEN': token }
+        })
+    .then(res => {
+        if (res.ok) {
+          toast.success("Course successfully dropped", {
+              position: toast.POSITION.BOTTOM_LEFT
+          });
+          this.fetchCourses();
+        } else {
+          toast.error("Course drop failed", {
+              position: toast.POSITION.BOTTOM_LEFT
+          });
+          console.error('Delete http status =' + res.status);
+    }})
+      .catch(err => {
+        toast.error("Course drop failed", {
+              position: toast.POSITION.BOTTOM_LEFT
+        });
+        console.error(err);
+      }) 
+    } 
+  }
+*/
+  // Add course
+  addStudent = (student) => {
+    const token = Cookies.get('XSRF-TOKEN');
+ 
+    fetch(`${SERVER_URL}/student/new`,
+      { 
+        method: 'POST', 
+        headers: { 'Content-Type': 'application/json',
+                   'X-XSRF-TOKEN': token  }, 
+        body: JSON.stringify(student)
+      })
+    .then(res => {
+        if (res.ok) {
+          toast.success("Student successfully added", {
+              position: toast.POSITION.BOTTOM_LEFT
+          });
+          this.fetchStudents();
+        } else {
+          toast.error("Error when adding", {
+              position: toast.POSITION.BOTTOM_LEFT
+          });
+          console.error('Post http status =' + res.status);
+        }})
+    .catch(err => {
+      toast.error("Error when adding", {
+            position: toast.POSITION.BOTTOM_LEFT
+        });
+        console.error(err);
+    })
+  } 
+
+  render() {
+     const columns = [
+      { field: 'name', headerName: 'Name', width: 400 },
+      { field: 'email', headerName: 'Email', width: 125 },
+      { field: 'status', headerName: 'Status', width: 200 },
+      { field: 'status_code', headerName: 'Status Code', width: 150 },
+      ];
+  
+  return(
+      <div>
+          <AppBar position="static" color="default">
+            <Toolbar>
+               <Typography variant="h6" color="inherit">
+                  { 'Students'}
+                </Typography>
+            </Toolbar>
+          </AppBar>
+          <div className="App">
+            <div style={{width:'100%'}}>
+                For DEBUG:  display state.
+                {JSON.stringify(this.state)}
+            </div>
+            <Grid container>
+              <Grid item>
+			    <ButtonGroup>
+                  <AddStudent addStudent={this.addStudent}  />
+				</ButtonGroup>
+              </Grid>
+            </Grid>
+            <div style={{ height: 400, width: '100%' }}>
+              <DataGrid rows={this.state.courses} columns={columns} />
+            </div>
+            <ToastContainer autoClose={1500} />   
+          </div>
+      </div>
+      ); 
+  }
+}
+
+export default StudList;
